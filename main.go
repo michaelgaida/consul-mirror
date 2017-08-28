dd63866e380f332535f789ecfa29d522fa9222d6
mgaida <michael.gaida@agoda.com>
2017-08-28 17:47:59 +0700

    changed cli parsing

--- main.go
+++ main.go
@@ -1,48 +1,92 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/michaelgaida/consul-mirror/configuration"
 	"github.com/michaelgaida/consul-mirror/consul"
 	"github.com/michaelgaida/consul-mirror/storage"
+	"github.com/urfave/cli"
 )
 
 func main() {
-	var (
-		validate = flag.String("validate", "", "configuration file to be validated")
-	)
+	app := cli.NewApp()
+	app.Name = "consul-mirror"
+	app.Version = "0.1.0"
 
-	flag.Parse()
-
-	if *validate != "" {
-		// check the new config
-		testConfiguration := configuration.GetConfig(*validate)
-		os.Exit(testConfiguration.ValidateConfiguration())
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "validate",
+			Value: "",
+			Usage: "configuration file to be validated",
+		},
+		cli.BoolFlag{
+			Name:  "verbose",
+			Usage: "Switch on the verbose mode",
+		},
 	}
 
-	config := configuration.GetConfig("config.json")
-	if config.Debug {
-		fmt.Printf(config.PrintDebug())
+	app.Commands = []cli.Command{
+		cli.Command{
+			Name:  "import",
+			Usage: "import from consul",
+		},
+		cli.Command{
+			Name:  "export",
+			Usage: "export from consul",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "dc",
+					Usage: "keep the dcs",
+				},
+			},
+		},
 	}
 
-	// s := storage.Mssql{}
-	conn := storage.OpenConnection(config)
-	defer conn.Close()
+	app.Action = func(c *cli.Context) error {
+		if c.String("validate") != "" {
+			testConfiguration := configuration.GetConfig(c.String("validate"))
+			os.Exit(testConfiguration.ValidateConfiguration())
+		}
+		if (!(c.Bool("import"))) && (!(c.Bool("export"))) {
+			fmt.Println("Nothing to do")
+			os.Exit(0)
+		}
+		config := configuration.GetConfig("config.json")
+		if c.Bool("verbose") {
+			config.Debug = true
+		}
+		if config.Debug {
+			log.Printf(config.PrintDebug())
+		}
 
-	consul := consul.GetConsul(config)
-	kvs := consul.GetKVs("")
-	if kvs == nil {
-	}
-	conn.WriteKVs(kvs)
-	services := consul.GetServices()
-	if services == nil {
+		// s := storage.Mssql{}
+		conn := storage.OpenConnection(config)
+		// defer conn.Close()
 
-	}
-	nodes := consul.GetNodes()
-	if nodes == nil {
+		consul := consul.GetConsul(config)
 
+		dcs := consul.GetDCs()
+
+		if c.Bool("export") {
+			kvs := consul.GetKVs("", dcs)
+			if kvs == nil {
+			}
+			conn.WriteKVs(kvs)
+		}
+
+		if c.Bool("import") {
+			kvs, _ := conn.GetKVs()
+			err := consul.WriteKVs(kvs, c.Bool("dc"))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		return nil
 	}
+
+	app.Run(os.Args)
 }
